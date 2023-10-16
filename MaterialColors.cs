b9d99e990649e0ef8c35ed5f7e554e576fd256b8
Oz <686285+ozottin@users.noreply.github.com>
2023-10-16 00:22:08 -0700

    Create MaterialColors.cs

---
+++ MaterialColors.cs
@@ -0,0 +1,218 @@
+public class MaterialColors
+{
+    // Red
+    public static Color Red50 => new Color(1.000f, 0.921f, 0.933f);
+    public static Color Red100 => new Color(1.000f, 0.800f, 0.800f);
+    public static Color Red200 => new Color(0.996f, 0.686f, 0.710f);
+    public static Color Red300 => new Color(0.992f, 0.455f, 0.467f);
+    public static Color Red400 => new Color(0.988f, 0.278f, 0.322f);
+    public static Color Red500 => new Color(0.984f, 0.224f, 0.271f);
+    public static Color Red600 => new Color(0.961f, 0.184f, 0.220f);
+    public static Color Red700 => new Color(0.933f, 0.161f, 0.184f);
+    public static Color Red800 => new Color(0.906f, 0.118f, 0.149f);
+    public static Color Red900 => new Color(0.831f, 0.082f, 0.110f);
+
+    // Pink
+    public static Color Pink50 => new Color(1.000f, 0.922f, 0.937f);
+    public static Color Pink100 => new Color(1.000f, 0.765f, 0.820f);
+    public static Color Pink200 => new Color(1.000f, 0.600f, 0.713f);
+    public static Color Pink300 => new Color(0.992f, 0.420f, 0.635f);
+    public static Color Pink400 => new Color(0.984f, 0.278f, 0.584f);
+    public static Color Pink500 => new Color(0.976f, 0.153f, 0.525f);
+    public static Color Pink600 => new Color(0.957f, 0.110f, 0.471f);
+    public static Color Pink700 => new Color(0.933f, 0.090f, 0.427f);
+    public static Color Pink800 => new Color(0.910f, 0.071f, 0.384f);
+    public static Color Pink900 => new Color(0.831f, 0.051f, 0.310f);
+
+    // Purple
+    public static Color Purple50 => new Color(0.953f, 0.898f, 0.961f);
+    public static Color Purple100 => new Color(0.882f, 0.741f, 0.918f);
+    public static Color Purple200 => new Color(0.812f, 0.576f, 0.863f);
+    public static Color Purple300 => new Color(0.741f, 0.408f, 0.808f);
+    public static Color Purple400 => new Color(0.671f, 0.247f, 0.749f);
+    public static Color Purple500 => new Color(0.612f, 0.153f, 0.690f);
+    public static Color Purple600 => new Color(0.557f, 0.110f, 0.631f);
+    public static Color Purple700 => new Color(0.482f, 0.090f, 0.561f);
+    public static Color Purple800 => new Color(0.416f, 0.071f, 0.490f);
+    public static Color Purple900 => new Color(0.290f, 0.051f, 0.353f);
+
+    // Deep Purple
+    public static Color DeepPurple50 => new Color(0.929f, 0.918f, 0.969f);
+    public static Color DeepPurple100 => new Color(0.820f, 0.773f, 0.918f);
+    public static Color DeepPurple200 => new Color(0.702f, 0.616f, 0.863f);
+    public static Color DeepPurple300 => new Color(0.584f, 0.455f, 0.808f);
+    public static Color DeepPurple400 => new Color(0.494f, 0.341f, 0.749f);
+    public static Color DeepPurple500 => new Color(0.420f, 0.286f, 0.690f);
+    public static Color DeepPurple600 => new Color(0.365f, 0.239f, 0.631f);
+    public static Color DeepPurple700 => new Color(0.310f, 0.196f, 0.561f);
+    public static Color DeepPurple800 => new Color(0.259f, 0.157f, 0.490f);
+    public static Color DeepPurple900 => new Color(0.188f, 0.118f, 0.353f);
+
+    // Indigo
+    public static Color Indigo50 => new Color(0.914f, 0.918f, 0.964f);
+    public static Color Indigo100 => new Color(0.769f, 0.773f, 0.912f);
+    public static Color Indigo200 => new Color(0.616f, 0.616f, 0.859f);
+    public static Color Indigo300 => new Color(0.463f, 0.455f, 0.804f);
+    public static Color Indigo400 => new Color(0.353f, 0.341f, 0.745f);
+    public static Color Indigo500 => new Color(0.247f, 0.286f, 0.686f);
+    public static Color Indigo600 => new Color(0.220f, 0.239f, 0.631f);
+    public static Color Indigo700 => new Color(0.188f, 0.196f, 0.561f);
+    public static Color Indigo800 => new Color(0.157f, 0.157f, 0.490f);
+    public static Color Indigo900 => new Color(0.110f, 0.118f, 0.353f);
+
+    // Blue
+    public static Color Blue50 => new Color(0.890f, 0.925f, 0.976f);
+    public static Color Blue100 => new Color(0.733f, 0.871f, 0.984f);
+    public static Color Blue200 => new Color(0.565f, 0.816f, 0.988f);
+    public static Color Blue300 => new Color(0.392f, 0.761f, 0.992f);
+    public static Color Blue400 => new Color(0.259f, 0.710f, 0.988f);
+    public static Color Blue500 => new Color(0.129f, 0.651f, 0.988f);
+    public static Color Blue600 => new Color(0.118f, 0.600f, 0.957f);
+    public static Color Blue700 => new Color(0.102f, 0.525f, 0.918f);
+    public static Color Blue800 => new Color(0.086f, 0.451f, 0.878f);
+    public static Color Blue900 => new Color(0.063f, 0.302f, 0.749f);
+
+    // Light Blue
+    public static Color LightBlue50 => new Color(0.882f, 0.968f, 1.000f);
+    public static Color LightBlue100 => new Color(0.698f, 0.922f, 1.000f);
+    public static Color LightBlue200 => new Color(0.514f, 0.871f, 1.000f);
+    public static Color LightBlue300 => new Color(0.329f, 0.820f, 0.996f);
+    public static Color LightBlue400 => new Color(0.204f, 0.769f, 0.988f);
+    public static Color LightBlue500 => new Color(0.098f, 0.718f, 0.984f);
+    public static Color LightBlue600 => new Color(0.090f, 0.667f, 0.957f);
+    public static Color LightBlue700 => new Color(0.078f, 0.569f, 0.918f);
+    public static Color LightBlue800 => new Color(0.067f, 0.471f, 0.878f);
+    public static Color LightBlue900 => new Color(0.051f, 0.373f, 0.749f);
+
+    // Cyan
+    public static Color Cyan50 => new Color(0.878f, 1.000f, 1.000f);
+    public static Color Cyan100 => new Color(0.698f, 0.922f, 1.000f);
+    public static Color Cyan200 => new Color(0.514f, 0.871f, 1.000f);
+    public static Color Cyan300 => new Color(0.329f, 0.820f, 0.996f);
+    public static Color Cyan400 => new Color(0.204f, 0.769f, 0.988f);
+    public static Color Cyan500 => new Color(0.098f, 0.718f, 0.984f);
+    public static Color Cyan600 => new Color(0.090f, 0.667f, 0.957f);
+    public static Color Cyan700 => new Color(0.078f, 0.569f, 0.918f);
+    public static Color Cyan800 => new Color(0.067f, 0.471f, 0.878f);
+    public static Color Cyan900 => new Color(0.051f, 0.373f, 0.749f);
+
+    // Teal
+    public static Color Teal50 => new Color(0.878f, 0.973f, 0.976f);
+    public static Color Teal100 => new Color(0.675f, 0.902f, 0.910f);
+    public static Color Teal200 => new Color(0.459f, 0.831f, 0.847f);
+    public static Color Teal300 => new Color(0.243f, 0.761f, 0.784f);
+    public static Color Teal400 => new Color(0.102f, 0.714f, 0.733f);
+    public static Color Teal500 => new Color(0.000f, 0.675f, 0.659f);
+    public static Color Teal600 => new Color(0.000f, 0.631f, 0.604f);
+    public static Color Teal700 => new Color(0.000f, 0.553f, 0.522f);
+    public static Color Teal800 => new Color(0.000f, 0.475f, 0.439f);
+    public static Color Teal900 => new Color(0.000f, 0.376f, 0.318f);
+
+    // Green
+    public static Color Green50 => new Color(0.867f, 0.957f, 0.922f);
+    public static Color Green100 => new Color(0.675f, 0.875f, 0.816f);
+    public static Color Green200 => new Color(0.482f, 0.792f, 0.710f);
+    public static Color Green300 => new Color(0.290f, 0.710f, 0.604f);
+    public static Color Green400 => new Color(0.161f, 0.655f, 0.514f);
+    public static Color Green500 => new Color(0.129f, 0.588f, 0.455f);
+    public static Color Green600 => new Color(0.000f, 0.533f, 0.376f);
+    public static Color Green700 => new Color(0.000f, 0.463f, 0.318f);
+    public static Color Green800 => new Color(0.000f, 0.396f, 0.259f);
+    public static Color Green900 => new Color(0.000f, 0.318f, 0.204f);
+
+    // Light Green
+    public static Color LightGreen50 => new Color(0.925f, 0.969f, 0.910f);
+    public static Color LightGreen100 => new Color(0.776f, 0.914f, 0.741f);
+    public static Color LightGreen200 => new Color(0.620f, 0.855f, 0.576f);
+    public static Color LightGreen300 => new Color(0.467f, 0.796f, 0.408f);
+    public static Color LightGreen400 => new Color(0.365f, 0.753f, 0.278f);
+    public static Color LightGreen500 => new Color(0.263f, 0.710f, 0.153f);
+    public static Color LightGreen600 => new Color(0.227f, 0.659f, 0.110f);
+    public static Color LightGreen700 => new Color(0.192f, 0.573f, 0.090f);
+    public static Color LightGreen800 => new Color(0.161f, 0.486f, 0.071f);
+    public static Color LightGreen900 => new Color(0.118f, 0.361f, 0.051f);
+
+    // Lime
+    public static Color Lime50 => new Color(0.969f, 0.980f, 0.851f);
+    public static Color Lime100 => new Color(0.918f, 0.961f, 0.698f);
+    public static Color Lime200 => new Color(0.863f, 0.941f, 0.541f);
+    public static Color Lime300 => new Color(0.804f, 0.922f, 0.384f);
+    public static Color Lime400 => new Color(0.769f, 0.898f, 0.259f);
+    public static Color Lime500 => new Color(0.733f, 0.875f, 0.145f);
+    public static Color Lime600 => new Color(0.714f, 0.812f, 0.129f);
+    public static Color Lime700 => new Color(0.627f, 0.710f, 0.110f);
+    public static Color Lime800 => new Color(0.541f, 0.604f, 0.090f);
+    public static Color Lime900 => new Color(0.431f, 0.475f, 0.067f);
+    
+    // Yellow
+    public static Color Yellow50 => new Color(1.000f, 0.992f, 0.749f);
+    public static Color Yellow100 => new Color(1.000f, 0.984f, 0.592f);
+    public static Color Yellow200 => new Color(1.000f, 0.976f, 0.435f);
+    public static Color Yellow300 => new Color(1.000f, 0.965f, 0.278f);
+    public static Color Yellow400 => new Color(1.000f, 0.957f, 0.176f);
+    public static Color Yellow500 => new Color(1.000f, 0.953f, 0.122f);
+    public static Color Yellow600 => new Color(0.992f, 0.886f, 0.110f);
+    public static Color Yellow700 => new Color(0.984f, 0.820f, 0.098f);
+    public static Color Yellow800 => new Color(0.976f, 0.753f, 0.086f);
+    public static Color Yellow900 => new Color(0.961f, 0.620f, 0.063f);
+    
+    // Amber
+    public static Color Amber50 => new Color(1.000f, 0.969f, 0.616f);
+    public static Color Amber100 => new Color(1.000f, 0.945f, 0.345f);
+    public static Color Amber200 => new Color(1.000f, 0.922f, 0.173f);
+    public static Color Amber300 => new Color(1.000f, 0.898f, 0.000f);
+    public static Color Amber400 => new Color(1.000f, 0.878f, 0.000f);
+    public static Color Amber500 => new Color(1.000f, 0.835f, 0.000f);
+    public static Color Amber600 => new Color(0.992f, 0.776f, 0.000f);
+    public static Color Amber700 => new Color(0.984f, 0.714f, 0.000f);
+    public static Color Amber800 => new Color(0.976f, 0.651f, 0.000f);
+    public static Color Amber900 => new Color(0.961f, 0.525f, 0.000f);
+    
+    // Orange
+    public static Color Orange50 => new Color(1.000f, 0.941f, 0.855f);
+    public static Color Orange100 => new Color(1.000f, 0.875f, 0.710f);
+    public static Color Orange200 => new Color(1.000f, 0.808f, 0.565f);
+    public static Color Orange300 => new Color(1.000f, 0.741f, 0.420f);
+    public static Color Orange400 => new Color(1.000f, 0.678f, 0.310f);
+    public static Color Orange500 => new Color(1.000f, 0.612f, 0.208f);
+    public static Color Orange600 => new Color(1.000f, 0.557f, 0.176f);
+    public static Color Orange700 => new Color(1.000f, 0.478f, 0.141f);
+    public static Color Orange800 => new Color(1.000f, 0.400f, 0.106f);
+    public static Color Orange900 => new Color(0.984f, 0.278f, 0.075f);
+
+    // Deep Orange
+    public static Color DeepOrange50 => new Color(1.000f, 0.937f, 0.918f);
+    public static Color DeepOrange100 => new Color(1.000f, 0.871f, 0.784f);
+    public static Color DeepOrange200 => new Color(1.000f, 0.804f, 0.647f);
+    public static Color DeepOrange300 => new Color(1.000f, 0.733f, 0.514f);
+    public static Color DeepOrange400 => new Color(1.000f, 0.659f, 0.396f);
+    public static Color DeepOrange500 => new Color(1.000f, 0.588f, 0.247f);
+    public static Color DeepOrange600 => new Color(1.000f, 0.533f, 0.220f);
+    public static Color DeepOrange700 => new Color(1.000f, 0.455f, 0.196f);
+    public static Color DeepOrange800 => new Color(1.000f, 0.376f, 0.173f);
+    public static Color DeepOrange900 => new Color(1.000f, 0.235f, 0.141f);
+
+    // Brown
+    public static Color Brown50 => new Color(0.937f, 0.925f, 0.910f);
+    public static Color Brown100 => new Color(0.843f, 0.808f, 0.784f);
+    public static Color Brown200 => new Color(0.737f, 0.678f, 0.655f);
+    public static Color Brown300 => new Color(0.631f, 0.549f, 0.525f);
+    public static Color Brown400 => new Color(0.553f, 0.459f, 0.451f);
+    public static Color Brown500 => new Color(0.475f, 0.373f, 0.376f);
+    public static Color Brown600 => new Color(0.427f, 0.333f, 0.310f);
+    public static Color Brown700 => new Color(0.365f, 0.278f, 0.247f);
+    public static Color Brown800 => new Color(0.310f, 0.224f, 0.184f);
+    public static Color Brown900 => new Color(0.216f, 0.122f, 0.110f);
+
+    // Grey
+    public static Color Grey50 => new Color(0.980f, 0.980f, 0.980f);
+    public static Color Grey100 => new Color(0.961f, 0.961f, 0.961f);
+    public static Color Grey200 => new Color(0.933f, 0.933f, 0.933f);
+    public static Color Grey300 => new Color(0.878f, 0.878f, 0.878f);
+    public static Color Grey400 => new Color(0.741f, 0.741f, 0.741f);
+    public static Color Grey500 => new Color(0.620f, 0.620f, 0.620f);
+    public static Color Grey600 => new Color(0.561f, 0.561f, 0.561f);
+    public static Color Grey700 => new Color(0.475f, 0.475f, 0.475f);
+    public static Color Grey800 => new Color(0.384f, 0.384f, 0.384f);
+    public static Color Grey900 => new Color(0.259f, 0.259f, 0.259f);
+}
